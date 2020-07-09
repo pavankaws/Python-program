3a8a256582807e4423f0c6d483fde3abb9f59b49
pavankaws <36111807+pavankaws@users.noreply.github.com>
2020-07-09 21:35:48 +0530

    Arithmetic-operators
    Arithmetic operators in python

---
+++ Arithmetic-operators.py
@@ -0,0 +1,23 @@
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10%3)
+print(10**3)
+## argumented assignment operator
+x = 10
+x = x+3
+x+=3
+print(x)
+x = 10
+x-=3
+print(x)
+x= 10 +3* 2
+print(x)
+Ans: 16 # becoz of opeartioanl [precendence BEDMAS
+x = 10+ 3*2 ** 2
+print(x)
+Ans:22
+x = (2+3) * 10 - 3
+print(x)
